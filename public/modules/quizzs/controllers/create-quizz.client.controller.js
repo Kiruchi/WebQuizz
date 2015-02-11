d0fe2d5ceb6f49b9ea07e2b50fb1e3c957fe8473
Pierre Martin <pierre.martin49@gmail.com>
2015-02-11 17:48:51 +0100

    Ajout Rating

--- public/modules/quizzs/controllers/create-quizz.client.controller.js
+++ public/modules/quizzs/controllers/create-quizz.client.controller.js
@@ -3,6 +3,24 @@
 angular.module('quizzs').controller('CreateQuizzController', ['$scope',
 	function($scope) {
 
+		// DEBUT RATE
+		$scope.max = 5;
+		$scope.isReadonly = false;
+
+		$scope.hoveringOver = function(value) {
+		$scope.overStar = value;
+		$scope.percent = 100 * (value / $scope.max);
+		};
+
+		$scope.ratingStates = [
+		{stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle'},
+		{stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty'},
+		{stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle'},
+		{stateOn: 'glyphicon-heart'},
+		{stateOff: 'glyphicon-off'}
+		];
+	    // FIN RATE
+
 		$scope.questions=[];
 
 		$scope.step = true;
