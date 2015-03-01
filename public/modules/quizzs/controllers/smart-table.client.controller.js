5059d8a5fe4f126a1cf74654ef1183d41d0aa416
Pierre Martin <pierre.martin49@gmail.com>
2015-03-01 16:23:05 +0100

    WebDesign liste quizzs

--- public/modules/quizzs/controllers/smart-table.client.controller.js
+++ public/modules/quizzs/controllers/smart-table.client.controller.js
@@ -1,13 +1,27 @@
 'use strict';
 
-angular.module('quizzs').controller('TableIntelController', ['$scope',
-	function($scope) {
+angular.module('quizzs').controller('TableIntelController', ['$scope', '$location',
+	function($scope, $location) {
+		$scope.clickEdit = false;
+		$scope.displayedCollection = [].concat($scope.quizzs);
 
-    //remove to the real data holder
-    $scope.removeItem = function removeItem(row) {
-        var index = $scope.rowCollection.indexOf(row);
-        if (index !== -1) {
-            $scope.rowCollection.splice(index, 1);
-        }
-    };
+	    //remove to the real data holder
+	    $scope.removeItem = function removeItem(row) {
+	        var index = $scope.quizzs.indexOf(row);
+	        if (index !== -1) {
+	            $scope.displayedCollection.splice(index, 1);
+	        }
+	    };
+
+	    $scope.itemsByPage=10;
+
+	    $scope.editClick = function() {
+		  $scope.clickEdit = true;
+		};
+
+	    $scope.showQuizz = function(quizz) {
+	    	if(!$scope.clickEdit) {
+	    		$location.path('quizzs/' + quizz._id);
+	    	}
+		};
 }]);
