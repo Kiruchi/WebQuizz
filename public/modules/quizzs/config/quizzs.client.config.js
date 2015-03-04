e9c529dc8c74fab289333b07f07d549f8aaeeaa8
Pierre Martin <pierre.martin49@gmail.com>
2015-03-04 14:11:46 +0100

    Mise en français du menu quizzs

--- public/modules/quizzs/config/quizzs.client.config.js
+++ public/modules/quizzs/config/quizzs.client.config.js
@@ -5,7 +5,7 @@ angular.module('quizzs').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
 		Menus.addMenuItem('topbar', 'Quizzs', 'quizzs', 'dropdown', '/quizzs(/create)?');
-		Menus.addSubMenuItem('topbar', 'quizzs', 'List Quizzs', 'quizzs');
-		Menus.addSubMenuItem('topbar', 'quizzs', 'New Quizz', 'quizzs/create');
+		Menus.addSubMenuItem('topbar', 'quizzs', 'Liste des quizzs', 'quizzs');
+		Menus.addSubMenuItem('topbar', 'quizzs', 'Nouveau quizz', 'quizzs/create');
 	}
 ]);
