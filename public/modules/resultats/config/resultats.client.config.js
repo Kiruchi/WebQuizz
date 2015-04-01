219f20a6d05a366343ee1d6e3c055b2bb957541a
Pierre Martin <pierre.martin49@gmail.com>
2015-04-01 18:12:09 +0200

    Modif des menus

--- public/modules/resultats/config/resultats.client.config.js
+++ public/modules/resultats/config/resultats.client.config.js
@@ -4,8 +4,7 @@
 angular.module('resultats').run(['Menus',
 	function(Menus) {
 		// Set top bar menu items
-		Menus.addMenuItem('topbar', 'Resultats', 'resultats', 'dropdown', '/resultats(/create)?');
-		Menus.addSubMenuItem('topbar', 'resultats', 'List Resultats', 'resultats');
-		Menus.addSubMenuItem('topbar', 'resultats', 'New Resultat', 'resultats/create');
+		Menus.addMenuItem('topbar', 'Résultats', 'resultats', 'dropdown', '/resultats(/create)?');
+		Menus.addSubMenuItem('topbar', 'resultats', 'Liste des résultats', 'resultats');
 	}
 ]);
